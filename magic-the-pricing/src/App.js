41b5cc0a7a07f20a0a6bb1a6c227b97a9a7bc4da
vissicchioj <60723403+vissicchioj@users.noreply.github.com>
2024-03-07 19:41:30 -0500

    Game Complete!
    Future concerns: update styling, speed up fetching, etc

--- magic-the-pricing/src/App.js
+++ magic-the-pricing/src/App.js
@@ -6,15 +6,17 @@ function App() {
   const [card1, setCard1] = useState(null);
   const [card2, setCard2] = useState(null);
   const [result, setResult] = useState('');
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://api.scryfall.com/cards/random');
+        const response = await axios.get('https://api.scryfall.com/cards/random?q=%28game%3Apaper%29+usd%3E0+%28image_uris%3Anormal%29');
         setCard1(response.data);
 
-        const response2 = await axios.get('https://api.scryfall.com/cards/random');
+        const response2 = await axios.get('https://api.scryfall.com/cards/random?q=%28game%3Apaper%29+usd%3E0+%28image_uris%3Anormal%29');
         setCard2(response2.data);
 
       } catch (error){
@@ -28,32 +30,53 @@ function App() {
     card1.prices.usd >= card2.prices.usd ? setResult('card 1 is more expensive ' + card1.prices.usd) : setResult('card 2 is more expensive ' + card2.prices.usd);
   }
   function nextCards() {
+    setCard1(null);
+    setCard2(null);
     setCounter(prevCounter => prevCounter+1);
+    setResult('')
+  }
+
+  function correct() {
+    setResult('Correct');
+    setScore(prevScore => prevScore + 1);
+  }
+  function incorrect() {
+    setResult('Incorrect');
+    setHighScore(prevHighscore => score > prevHighscore ? prevHighscore = score : prevHighscore);
+    setScore(0);
+  }
+
+  function card1Clicked() {
+    card1.prices.usd >= card2.prices.usd ? correct() : incorrect();
+  }
+  function card2Clicked() {
+    card2.prices.usd >= card1.prices.usd ? correct() : incorrect();
   }
 
 
   return (
     <div className="App">
+      <h1>Which card is more expensive?</h1>
+      <h3>Score: {score}</h3>
+      <h3>High Score: {highScore}</h3>
       <div>
         {card1 && card2 ? (
-          <div>
-            <img src = {card1.image_uris.normal} />
-            <p>{card1.prices.usd}</p>
-            <img src = {card2.image_uris.normal} />
-            <p>{card2.prices.usd}</p>
+          <div className='cards'>
+            <img src = {card1.image_uris.normal} onClick={card1Clicked} className='card1'/>
+            <img src = {card2.image_uris.normal} onClick={card2Clicked} className='card2'/>
           </div>
           
         ) : 
         (<p>loading...</p>)}
       </div>
-
-      <button onClick={comparePrices}>
-          comparePrices
-      </button>
-      <button onClick={nextCards}>
-          next
+      
+      <div className='menu'>
+      <button disabled={result===''} onClick={nextCards}>
+          Next
       </button>
       <p>{result}</p>
+      {result !== '' ? <div><p>{card1.name}: {card1.prices.usd}</p> <p>{card2.name}: {card2.prices.usd}</p></div>: ''}
+    </div>
     </div>
     
   );
